6ab066ba04babfb059c8f7975fdd3aa193e6fe9c
KC Jones <kc@kctec.net>
2019-02-12 15:12:01 -0500

    Updates

---
+++ MylarSideCar/Manager/TorznabManager.cs
@@ -0,0 +1,23 @@
+﻿using MylarSideCar.Manager.Configs;
+using MylarSideCar.Model.ComicVine;
+using Newtonsoft.Json;
+using RestSharp;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MylarSideCar.Manager
+{
+    public class TorzNabManager
+    {
+        
+
+        private static RestClient GetRestClient(string URI)
+        {
+            return new RestClient(URI);
+        }
+ 
+   
+
+
+    }
+}
